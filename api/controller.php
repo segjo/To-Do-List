f3468c4f8cc39c60cc37ce45b3f6762ba66084d7
Jonas S <jonas_segessemann@hotmail.com>
2018-10-17 21:55:06 +0200

    .

--- api/controller.php
+++ api/controller.php
@@ -1,28 +1,81 @@
 <?php
-    session_start();
-    if(!isset($_SESSION['login'])) {
-        header('LOCATION:../tmpLogin.php'); die();
-    }
-echo "controller";
+
+session_start();
+if (!isset($_SESSION['login'])) {
+    header('LOCATION:../tmpLogin.php');
+    die();
+}
+require_once('../../credentials.php');
+//require_once('models/Items.php');
+require_once('models/Profile.php');
+//---------------KONSTANTEN-----------------
+define("URI_1", 3);
+define("URI_2", 4);
+define("URI_REQ", $_SERVER[REQUEST_METHOD]);
+
+ini_set('display_errors', 'on');
 
 
+
+//$itemModel= new Item();
+//$profileModel= new Profile();
+
+
+$url = $_SERVER[REQUEST_URI];
+$values = parse_url($url);
+$urlPaths = $host = explode('/', $values['path']);
+
+try {
+
+    $dbh = new PDO('mysql:host=' . DB_HOST . ';dbname=' . DB_NAME, DB_USERNAME, DB_PASSWORD);
+} catch (PDOException $e) {
+    print "Error!: " . $e->getMessage() . "<br/>";
+    die();
+}
+
+switch ($urlPaths[URI_1]) {
+    case 'profile':
+
+        $profileModel = new Profile($dbh);
+        if ($urlPaths[URI_2] == 'create' && URI_REQ == 'POST') {
+            $returnValue = $profileModel->create();
+        }
+        if ($urlPaths[URI_2] == 'login' && URI_REQ == 'POST') {
+            $returnValue = $profileModel->login();
+        }
+        if (URI_REQ == 'DELETE') {
+            $returnValue = $profileModel->delete();
+        }
+        echo $profileModel->create();
+
+
+        break;
+    case 'list':
+        $this->add_user();
+        break;
+    case 'item':
+        $this->update_user();
+        break;
+}
 
 /*
-        switch($_SERVER['REQUEST_METHOD']){
-            case 'GET':
-                $this->get_user();
-             break;
-            case 'POST':
-                 $this->add_user();
-             break;
-            case 'PUT':
-                 $this->update_user();
-             break;
-            case 'DELETE':
-                 $this->delete_user();
-             break;
-        }*/
-           
-    
+  switch($_SERVER['REQUEST_METHOD']){
+  case 'GET':
+  $this->get_user();
+  break;
+  case 'POST':
+  $this->add_user();
+  break;
+  case 'PUT':
+  $this->update_user();
+  break;
+  case 'DELETE':
+  $this->delete_user();
+  break;
+  } */
+
+
+$dbh = null;
+
 
 
--- api/models/Profile.php
+++ api/models/Profile.php
@@ -1,8 +1,50 @@
 <?php
 
-/* 
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
- */
+class Profile {
 
+    private $db;
+
+    function __construct(PDO $dbh) {
+        $this->db = $dbh;
+    }
+
+    public function create() {//TODO
+        $email = "dw";
+
+        $sql = "SELECT * FROM User WHERE Email = '" . $email . "'";
+
+        $select = $this->db->prepare($sql);
+        $select->execute();
+        $count = $select->rowCount();
+
+        if ($count > 0) {
+            //Email existiert bereits
+            return false;
+        }
+
+
+
+
+        $statement = $this->db->prepare("INSERT INTO User (UserId, Name, LastName, Email, EmailActivated, UserName, EncryptedPassword, Salt, CreatedAt, UpdatedAt) VALUES (?, ?, ?, ?, ?,?, ?, ?, ?, ?)");
+        $insert = $statement->execute(array(NULL, 'adawd', 'awdaw', 'adwd2w@adwaw', '0', 'awdawd', 'fefe', 'efef', '2018-10-17', NULL));
+        if ($insert) {
+            echo 'created Profile ';
+            return true;
+        } else {
+            return false;
+        }
+
+        echo 'created Profile ' . $value;
+    }
+
+    public function delete() {//TODO
+        echo 'delete Profile';
+    }
+
+    public function login() {//TODO
+        foreach ($this->db->query('SELECT * from User') as $row) {
+            var_dump($row);
+        }
+    }
+
+}
